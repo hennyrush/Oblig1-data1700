64e4057cd6fe405bfe6eaa33d966997ee131f641
Henny Rushfeldt <rushfeldt8@gmail.com>
2024-02-15 15:08:19 +0100

    Endret i html og js

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,129 +1,98 @@
-const billetter=[];
+const bestilling = [];
+function kjopBillett() {
+    const filmNavn = document.getElementById("filmNavn").value;
+    const antallBilletter = document.getElementById("antallBilletter").value;
+    const fornavn = document.getElementById("fornavn").value;
+    const etternavn = document.getElementById("etternavn").value;
+    const telefonnr = document.getElementById("telefonNr").value;
+    const epost = document.getElementById("epost").value;
 
-function kjopBillett(){
+    // verdiene går gjennom validering
 
-    const film=document.getElementById("slctFilm");
-    const antall=document.getElementById("inpAntall");
-    const fornavn=document.getElementById("inpFornavn");
-    const etternavn=document.getElementById("inpEtternavn");
-    const tlfnr=document.getElementById("inpTelefonnr");
-    const epost=document.getElementById("inpEpost");
-
-    let feilFilm=document.getElementById("nullFilm");
-    let feilAntall=document.getElementById("nullAntall");
-    let feilFornavn=document.getElementById("nullFornavn");
-    let feilEtternavn=document.getElementById("nullEtternavn");
-    let feilTlfnr=document.getElementById("nullTelefonnr");
-    let feilEpost=document.getElementById("nullEpost");
-
-
-    const filmer=document.getElementById("tblFilmer");
-
-    const sjekkTlfnr = Number(tlfnr.value);
-
-
-
-    if (film.value === "Velg Film") {
-        feilFilm.innerHTML = "Du må velge en film";
+    //film
+    if (filmNavn === "Velg film her") {
+        document.getElementById("feilFilmNavn").textContent = " Velg en film";
+    } else {
+        document.getElementById("feilFilmNavn").textContent = "";
+    }
+    //billetter
+    if (!antallBilletter || isNaN(antallBilletter) || antallBilletter <= 0) {
+        document.getElementById("feilAntallBilletter").textContent = "Skriv antall billettter";
+    } else {
+        document.getElementById("feilAntallBilletter").textContent = "";
+    }
+    //fornavn
+    if (!fornavn) {
+        document.getElementById("feilFornavn").textContent = "Fyll ut fornavn";
+    } else {
+        document.getElementById("feilFornavn").textContent = "";
+    }
+    //etternavn
+    if (!etternavn) {
+        document.getElementById("feilEtternavn").textContent = " Skriv inn etternavn";
+    } else {
+        document.getElementById("feilEtternavn").textContent = "";
+    }
+    //telefonnummer: https://stackoverflow.com/questions/73656999/regex-to-validate-only-digits-from-0-9-maximum-of-8-digits-till-a-dot-and-only
+    if (!telefonnr || !/^[0-9]{8}$/.test(telefonnr)) {
+        document.getElementById("feilTelefonNr").textContent = "Skriv et telefonnummer med 8 tall";
+    } else {
+        document.getElementById("feilTelefonNr").textContent = "";
 
     }
+    //epost: https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
 
-    if (antall.value === "") {
-        feilAntall.innerHTML = "Du må skrive noe i antall";
-
-
+    if (!epost || !/^[^\s*]+@[^\s@]+\.[^\s@]+$/.test(epost)) {
+        document.getElementById("feilEpost").textContent = "Skriv en fungerende epostadresse";
+    } else {
+        document.getElementById("feilEpost").textContent = "";
     }
+    //variabelene går inn i en billett
+    const nyBillett = {
+        Film: filmNavn,
+        AntallBilletter: antallBilletter,
+        Fornavn: fornavn,
+        Etternavn: etternavn,
+        Telefonnr: telefonnr,
+        Epost: epost
+    };
 
-    if (fornavn.value === "") {
-        feilFornavn.innerHTML = "Du må skrive noe i fornavn";
-
-
-    }
-
-    if (etternavn.value === "") {
-        feilEtternavn.innerHTML = "Du må skrive noe i etternavn";
-
-
-    }
-
-    if (isNaN(sjekkTlfnr)) {
-        feilTlfnr.innerHTML = "Du må skrive inn tall i tlfnr ";
-
-
-    }
-
-    if (tlfnr.value === "") {
-        feilTlfnr.innerHTML = "Du må skrive noe i telefonnummer, og ";
-
-
-    }
-
-    if (tlfnr.value.length !== 8) {
-        feilTlfnr.innerHTML += "Du må skrive et telefonnummer med 8 siffer";
-
-    }
-
-
-    if (epost.value === "") {
-        feilEpost.innerHTML = "Du må skrive noe i Epost";
-
-    }
-    else {
-        billetter.push({
-            Film: film.value,
-            Antall: antall.value,
-            Fornavn: fornavn.value,
-            Etternavn: etternavn.value,
-            Telefonnr: tlfnr.value,
-            Epost: epost.value
-        });
-
-        filmer.innerHTML = "<tr><th>Film</th>" + "<th>Antall</th>" + "<th>Fornavn</th>" + "<th>Etternavn</th>" + "<th>Telefonnummer</th>" + "<th>Epost</th></tr>";
-
-        for (let i = 0; i < billetter.length; i++) {
-            filmer.innerHTML += "<tr><td>" + billetter[i].Film + "</td><td>" + billetter[i].Antall + "</td><td>" + billetter[i].Fornavn + "</td><td>" + billetter[i].Etternavn + "</td><td>" + billetter[i].Telefonnr + "</td><td>" + billetter[i].Epost + "</td></tr>";
-        }
-
-
-    }
-    if (film.value !=="Velg Film"){
-        feilFilm.innerHTML="";
-    }
-    if (antall.value !==""){
-        feilAntall.innerHTML="";
-    }
-    if (fornavn.value !==""){
-        feilFornavn.innerHTML="";
-    }
-    if(etternavn.value !=="") {
-        feilEtternavn.innerHTML = "";
-    }
-
-    if(tlfnr.value !=="" && tlfnr.value.length==8){
-        feilTlfnr.innerHTML="";
-    }
-    if(epost.value !==""){
-        feilEpost.innerHTML="";
-    }
-    film.value="Velg Film";
-    antall.value="";
-    fornavn.value="";
-    etternavn.value="";
-    tlfnr.value="";
-    epost.value="";
-
+    //billett blir lagt til i arrayet
+    bestilling.push(nyBillett);
+    output();
+}
+function output(){
+    //billeten skrives ut
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerHTML;
+    bestilling.forEach(billett => {
+        const billettInfo = `
+            <p>
+                Film: ${billett.Film}<br>
+                Antall billetter: ${billett.AntallBilletter}<br>
+                Fornavn: ${billett.Fornavn}<br>
+                Etternavn: ${billett.Etternavn}<br>
+                Telefonnr: ${billett.Telefonnr}<br>
+                Epost: ${billett.Epost}
+            </p>
+        `;
+        outputDiv.innerHTML += billettInfo;
+    });
 
 }
+function slett() {
+    bestilling.length = 0;
+    output();
 
-const filmer=document.getElementById("tblFilmer");
-filmer.innerHTML="";
+    // sletter teksten i boksene
 
+    document.getElementById("filmNavn").value = "Velg film her";
+    document.getElementById("antallBilletter").value = "";
+    document.getElementById("fornavn").value = "";
+    document.getElementById("etternavn").value = "";
+    document.getElementById("telefonNr").value = "";
+    document.getElementById("epost").value = "";
 
-function slettBillett(){
-    const filmer=document.getElementById("tblFilmer");
-
-    billetter.splice(0,billetter.length);
-    filmer.innerHTML="";
-
-
+    //sletter billetten
+    document.getElementById("output").innerHTML = ""
 }
